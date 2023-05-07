904b939a47969b2bd6a3a626d20940608aa4d58f
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-05-08 02:09:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+function App() {
+  const handelAddUser = (e) => {
+    e.preventDefault()
+    const form = e.target
+    const name = form.name.value
+    const email = form.email.value
+    const user = { name, email }
+    console.log(user)
+    fetch(`http://localhost:5000/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+
+  }
+
+  return (
+    <>
+      <h1>Crud Operations</h1>
+      <form onSubmit={handelAddUser} >
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" name="addUser" value="Add User" />
+      </form>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Users.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Users = () => {
+    const loaderUsers = useLoaderData()
+    const [users, setUsers] = useState(loaderUsers)
+    const handelDelete = (_id) => {
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: "DELETE"
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.deletedCount > 0) {
+                    alert(`delete item successfully`)
+                    const remain = users.filter(user=>user._id !== _id)
+                    setUsers(remain)
+                }
+            })
+    }
+    return (
+        <div>
+            <h1>{users.length}</h1>
+            {
+                users.map(user => <p key={user._id}>{user.name}:{user.email}{user._id} <button onClick={() => handelDelete(user._id)} >X</button></p>
+
+                )
+            }
+        </div>
+    );
+};
+
+export default Users;
